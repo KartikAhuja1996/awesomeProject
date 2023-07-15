47a140364c47b6a2b6050df573d28ac77e632def
Kartik Ahuja <kartik.ahuja2@gmail.com>
2023-07-15 12:53:07 +0530

    version_1

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text,Linking, Platform,StatusBar } from 'react-native';
+
+import {colors} from './src/utils/colors';
+import { Focus } from './src/features/Focus';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Focus/>
     </View>
   );
 }
@@ -13,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: colors.darkblue,
+    paddingTop:Platform.OS = "android" ? StatusBar.currentHeight : 0,
+  }
 });
